820ab55fe214c318dd32f0241a002671b640a227
sarsh280 <111204926+sarsh280@users.noreply.github.com>
2022-11-30 16:51:44 -0500

    create pwm.v

---
+++ pwm.v
@@ -0,0 +1,21 @@
+
+module pwm(input clk, 
+output [3:0]pwm_out);
+
+reg [7:0] counter = 0;
+
+always@(posedge clk) begin
+	if(counter < 100) counter <= counter + 1;
+	else counter <= 0;
+end
+
+assign pwm_out[0] = (counter < 20) ? 1:0;
+
+assign pwm_out[1] = (counter < 40) ? 1:0;
+
+assign pwm_out[2] = (counter < 60) ? 1:0;
+
+assign pwm_out[3] = (counter < 80) ? 1:0;
+
+
+endmodule
